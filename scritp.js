83afa49577453c7fdbb8a7f03bea485bb74dbfdb
TM0relli <153144643+TM0relli@users.noreply.github.com>
2025-06-24 21:12:03 -0300

    scritp.js

--- scritp.js
+++ scritp.js
@@ -4,7 +4,7 @@ function mostrarPagina(pagina){
         conteudo.innerHTML = `
         <section> 
         <h2> Sobre mim</h2>
-        <p>Desenvolvedor com foco em Front-End, performance e resultados. Atualmente estou em busca de oportunidades para ganhar experiência prática na área, mas já venho estudando e desenvolvendo projetos próprios para otimizar sistemas e aumentar conversões.</p>
+        <p>Desenvolvedor com foco em Front-End, performance e resultados.</p>
         </section> `;
     }else if (pagina === "projetos") {
         conteudo.innerHTML = `
@@ -19,7 +19,7 @@ function mostrarPagina(pagina){
           <section>
             <h2>Vamos conversar?</h2>
             <p>Entre em contato por:</p>
-            <a href="mailto:email@exemplo.com" class="btn"> E-mail</a><br><br>
+            <a href="Seu Email" class="btn"> E-mail</a><br><br>
             <a href="https://www.linkedin.com/in/seuperfil" target="_blank" rel="noopener noreferrer" class="btn">
               <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" />
               LinkedIn
@@ -33,7 +33,7 @@ function mostrarPagina(pagina){
     }else{
         conteudo.innerHTML = `
         <header> 
-        <h1> Olá, sou <span class="destaque"> Tomas</span></h1>
+        <h1> Olá, sou <span class="destaque"> Seu Nome</span></h1>
         <p> Transformo ideias em soluções digitais que impactam negócios.</p>
         <button onclick="mostrarPagina('contato')" class="btn">Fale comigo></button>
         </header>`;
